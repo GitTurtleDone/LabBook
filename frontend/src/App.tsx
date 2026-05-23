963e06e7c2fe49a051bb7f13ae2ec0f5fa03c246
GitTurtleDone <giang2110@gmail.com>
2026-05-24 08:45:54 +1200

    Added EquipmentList.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,22 @@
-import { useState } from 'react'
-import { EquipmentList } from './components/EquipmentList'
+import { useState } from "react";
+import EquipmentList from "./components/EquipmentList";
+import type { Equipment } from './types';
 
 function App() {
-  
+  const [selected, setSelected] = useState<Equipment | null>(null);
   return (
     <div>
       <header>
         <h1>Lab Equipment Booking</h1>
       </header>
       <div className='grid'>
-        <EquipmentList/>
+        <EquipmentList
+          selectedId={selected?.id ?? null}
+          onSelect={setSelected}
+        />
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
