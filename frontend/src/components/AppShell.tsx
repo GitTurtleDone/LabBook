da8cf432c81d0d2e58a670b54f2b6a46d961d8b5
GitTurtleDone <giang2110@gmail.com>
2026-06-20 21:47:56 +1200

    Working on Zodsolver React Hook Form

--- frontend/src/components/AppShell.tsx
+++ frontend/src/components/AppShell.tsx
@@ -1,5 +1,14 @@
-import { Drawer, Box, List, ListItem, ListItemButton, ListItemText, Button, Menu} from "@mui/material";
-import { NavLink, Outlet} from "react-router-dom";
+import {
+  Drawer,
+  Box,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+  Button,
+  Menu,
+} from "@mui/material";
+import { NavLink, Outlet } from "react-router-dom";
 // import Drawer from "@mui/material/Drawer";
 // import Box from "@mui/material/Box";
 // import List from "@mui/material/Button";
@@ -9,65 +18,69 @@ import { NavLink, Outlet} from "react-router-dom";
 import { useState } from "react";
 import type { from } from "@apollo/client";
 // import { PersonIcon, PrecisionManufacturingIcon} from '@mui/icons-material';
-interface NavItem { label: string; path: string};
+interface NavItem {
+  label: string;
+  path: string;
+}
 import { IconButton } from "@mui/material";
-import MenuIcon from '@mui/icons-material/Menu';
+import MenuIcon from "@mui/icons-material/Menu";
 import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
 
-
-
 export default function AppShell() {
-    const [showSideBar, setShowSideBar] = useState(true);
-    const navItems: NavItem[] = [
-        { label: "Home", path: "/"},
-        { label: "Users", path: "/users"},
-        { label: "Equipment", path: "/equipment"},
-        { label: "Bookings", path: "/bookings"}
-    ]
-    return (
-    
+  const [showSideBar, setShowSideBar] = useState(true);
+  const navItems: NavItem[] = [
+    { label: "Home", path: "/" },
+    { label: "Users", path: "/users" },
+    { label: "Equipment", path: "/equipment" },
+    { label: "Bookings", path: "/bookings" },
+  ];
+  return (
     <div>
-        {/* <Button sx={{ width: 100, background: '#ffffff' }} onClick={() => setShowSideBar(!showSideBar)}>Side Bar</Button> */}
+      {/* <Button sx={{ width: 100, background: '#ffffff' }} onClick={() => setShowSideBar(!showSideBar)}>Side Bar</Button> */}
 
-        <Box sx={{ display: 'flex' }}>
-            <Drawer variant="persistent" anchor="left" open={showSideBar}>
-                <Box sx={{ width: 150 }}>
-                    <List>
-                        {navItems.map((i) => (
-                            <NavLink key={i.path} to={i.path} end>
-                                {({ isActive }: { isActive: boolean }) => (
-                                    <ListItem disablePadding>
-                                        <ListItemButton selected={isActive}>
-                                            <ListItemText primary={i.label} />
-                                        </ListItemButton>
-                                    </ListItem>
-                                )}
-                            </NavLink>
-                        ))}
-                    </List>
-                    
-                </Box>
-            </Drawer>
-            <IconButton
-                onClick={() => setShowSideBar(!showSideBar)}
-                sx={{
-                    position: 'fixed',
-                    left: showSideBar ? 110: 0,
-                    top: 8,
-                    trasition: 'left 1s',
-                    zIndex: 1300,
-                }}
-            >
-                {showSideBar ? <ChevronLeftIcon /> : <MenuIcon />}
-            </IconButton>
-            <Box component="main" sx={{ flexGrow: 1, backgroundColor: '#ffffff', minHeight: '100vh' }}>
-                <Outlet />
-            </Box>
+      <Box sx={{ display: "flex" }}>
+        <Drawer variant="persistent" anchor="left" open={showSideBar}>
+          <Box sx={{ width: 150 }}>
+            <List>
+              {navItems.map((i) => (
+                <NavLink key={i.path} to={i.path} end>
+                  {({ isActive }: { isActive: boolean }) => (
+                    <ListItem disablePadding>
+                      <ListItemButton selected={isActive}>
+                        <ListItemText primary={i.label} />
+                      </ListItemButton>
+                    </ListItem>
+                  )}
+                </NavLink>
+              ))}
+            </List>
+          </Box>
+        </Drawer>
+        <IconButton
+          onClick={() => setShowSideBar(!showSideBar)}
+          sx={{
+            position: "fixed",
+            left: showSideBar ? 110 : 0,
+            top: 8,
+            trasition: "left 1s",
+            zIndex: 1300,
+          }}
+        >
+          {showSideBar ? <ChevronLeftIcon /> : <MenuIcon />}
+        </IconButton>
+        <Box
+          component="main"
+          sx={{
+            marginLeft: showSideBar ? '150px' : 0,
+            flexGrow: 1,
+            backgroundColor: "#ffffff",
+            minHeight: "100vh",
+            transition: 'margin-left 0.3s'
+          }}
+        >
+          <Outlet />
         </Box>
-
+      </Box>
     </div>
-    
-);
-    
+  );
 }
-
--- frontend/src/components/EquipmentPage.tsx
+++ frontend/src/components/EquipmentPage.tsx
@@ -1,36 +1,71 @@
 import * as z from "zod";
 import { useForm } from "react-hook-form";
-import { Button, Stack, Typography, Input, OutlinedInput } from "@mui/material";
+import { Button, Stack, Typography, Input, OutlinedInput, Card } from "@mui/material";
 import { Category } from "@mui/icons-material";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { equipmentSchema, equipmentInputSchema, type Equipment, type EquipmentInput } from "../types";
 import { CREATE_EQUIPMENT } from "../graphql/queries";
 
-
-
+type ElementInput = {
+  label: string,
+  type: string,
+  elementKey: string,
+  disabled: boolean,
+  registered: boolean
+};
+type EquipmentLayout = ElementInput[];
+  
 
 
 export default function EquipmentPage() {
 
-  // const { register, handleSubmit, formState: { errors, isSubmitting},} = useForm<EquipmentInput>({
-  //   resolver: zodResolver(equipmentSchema),
-  // })
+  const equipmentLayout: EquipmentLayout = [
+    {label: 'Id', type: "string", elementKey: 'id', disabled: true, registered: true},
+    {label: 'Name', type: "string", elementKey: 'name', disabled: true, registered: true},
+  ]
+
+  const { 
+    register, 
+    handleSubmit, 
+    formState: { errors, isSubmitting}
+  } = useForm<EquipmentInput>({
+    resolver: zodResolver(equipmentSchema),
+    defaultValues: {
+      id: "",
+      name: "",
+      category: "Electrical",
+      description: "",
+      status: "AVAILABLE",
+      connectingStr: "",
+      model: "",
+      manufacturer: "",
+      serialNumber: "",
+      purchaseYear: "2006",
+      calibrationDue: "19-06-2027",
+      location: "",
+      requiresTraining: true,
+      imageUrl: "",
+      videoUrl: "",
+      documentationUrl: "",
+      notes: "",
+    }
+  })
   return (
     <div>
       {/* <h3>Equipment</h3> */}
-      <Typography variant="h3" sx={{pt:3}}>Equipment</Typography>
-      <Stack>
-        <Stack direction="row" pt={5} gap={5}>
-          <Typography >Equipment ID: </Typography>
-          <OutlinedInput type="text" size="small"></OutlinedInput>
+      <Card sx={{paddingLeft: 20}}>
+        <Typography variant="h3" sx={{pt:3}}>Equipment</Typography>
+        <Stack>
+          <Stack direction="row" pt={5} gap={10} >
+            {equipmentLayout.map((e) => {
+              <Typography>{e.label}</Typography>
+              <OutlinedInput e.registered ? {...register(e.elementKey)}: "" type={e.type}></OutlinedInput>
+            })}
+          </Stack>
+          <Button variant="contained" sx={{width: '10%', mt: 10}}>Create</Button>
         </Stack>
-        <Button variant="outlined" sx={{width: 1.5}}>Create</Button>
-      </Stack>
-      
-      
-
-      
-      
+      </Card>
+            
     </div>
     
   );
